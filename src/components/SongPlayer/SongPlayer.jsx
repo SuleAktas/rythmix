9c9c9f1b5fc617995851c77d04458b03445f439b
SuleAktas <suleaktas@live.nl>
2024-11-25 13:05:59 +0100

    fix:song player visibility change

--- src/components/SongPlayer/SongPlayer.jsx
+++ src/components/SongPlayer/SongPlayer.jsx
@@ -8,25 +8,27 @@ function SongPlayer({ openSongPlayer }) {
   const SONG_PIC = process.env.PUBLIC_URL + "/images/COLDPLAY.png";
   const { song } = useSong();
   return (
-    <div className="song-player-container" onClick={openSongPlayer}>
-      <div className="song-player-exp-box">
-        <div className="song-player-image-box">
-          <img
-            className="song-player-image"
-            src={SONG_PIC}
-            alt="Coldplay"
-          ></img>
+    song.name && (
+      <div className="song-player-container" onClick={openSongPlayer}>
+        <div className="song-player-exp-box">
+          <div className="song-player-image-box">
+            <img
+              className="song-player-image"
+              src={SONG_PIC}
+              alt="Coldplay"
+            ></img>
+          </div>
+          <div className="song-player-about">
+            <div className="song-player-name">{song.name} </div>
+            <div className="song-player-singer">{song.singer}</div>
+          </div>
         </div>
-        <div className="song-player-about">
-          <div className="song-player-name">{song.name} </div>
-          <div className="song-player-singer">{song.singer}</div>
+        <div className="song-player-actions">
+          <FavoriteBorderIcon fontSize="large"></FavoriteBorderIcon>
+          <PlayArrowIcon fontSize="large"></PlayArrowIcon>
         </div>
       </div>
-      <div className="song-player-actions">
-        <FavoriteBorderIcon fontSize="large"></FavoriteBorderIcon>
-        <PlayArrowIcon fontSize="large"></PlayArrowIcon>
-      </div>
-    </div>
+    )
   );
 }
 
