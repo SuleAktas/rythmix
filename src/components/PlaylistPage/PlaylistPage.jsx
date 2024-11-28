92a5177b1d0b8858da9329b18a1fc7eb4c2b4a73
SuleAktas <suleaktas@live.nl>
2024-11-28 14:39:15 +0100

    feat:get playlist dynamically with id

--- src/components/PlaylistPage/PlaylistPage.jsx
+++ src/components/PlaylistPage/PlaylistPage.jsx
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import PlaylistSong from "./components/PlaylistSong/PlaylistSong";
 import "./PlaylistPage.css";
 import PlayCircleFilledIcon from "@mui/icons-material/PlayCircleFilled";
 import PauseCircleFilledIcon from "@mui/icons-material/PauseCircleFilled";
 import { useSongApi } from "../../contexts/SongApiContext";
+import { useLikedPlaylists } from "../../contexts/LikedPlaylistContext";
 
 function PlaylistPage({ title }) {
-  const DAILYMIX4 = process.env.PUBLIC_URL + "/images/DAILYMIX4.png";
   const SPOTIFYLOGO = process.env.PUBLIC_URL + "/images/SPOTIFYLOGO.png";
   const BACKICON = process.env.PUBLIC_URL + "/images/BACKICON.png";
   const HEARTICON = process.env.PUBLIC_URL + "/images/HEARTICON.png";
@@ -16,24 +16,36 @@ function PlaylistPage({ title }) {
 
   const location = useLocation();
   const navigate = useNavigate();
+  const { tracks, loading, error, setTracks } = useSongApi();
+  const { setLikedPlaylists } = useLikedPlaylists();
+  const [songStatus, setSongStatus] = useState(true);
   const previousPage = location.state?.from || "/";
+  const album = location.state?.data;
 
   const handleNavigatePreviousPage = () => {
     console.log(location.state?.from);
     navigate(previousPage, { state: { from: window.location.pathname } });
   };
 
-  const [songStatus, setSongStatus] = useState(true);
   const handleSongStatus = () => {
     setSongStatus(!songStatus);
   };
-  const { tracks, loading, error, fetchTracks } = useSongApi();
-  const [tag, setTag] = useState("rock");
 
-  const handleTagChange = (newTag) => {
-    setTag(newTag);
-    fetchTracks(newTag);
+  const handleOnLike = () => {
+    setLikedPlaylists((prev) => [...prev, album]);
   };
+
+  useEffect(() => {
+    const fetchSongs = async () => {
+      const response = await fetch(
+        `https://api.jamendo.com/v3.0/tracks/?client_id=99c16ea4&album_id=${album.id}`
+      );
+      const data = await response.json();
+      setTracks(data.results);
+    };
+
+    fetchSongs();
+  }, [album.id]);
 
   if (loading) return <p>Loading tracks...</p>;
   if (error) return <p>{error}</p>;
@@ -50,11 +62,11 @@ function PlaylistPage({ title }) {
             ></img>
           </div>
           <div className="playlist-img">
-            <img src={DAILYMIX4} alt={title}></img>
+            <img src={tracks[0].album_image} alt={title}></img>
           </div>
 
           <div className="playlist-exp">
-            <h1>This Is Coldplay</h1>
+            <h1>{tracks[0].album_name}</h1>
             <p>The essential tracks,all in one playlist</p>
             <div className="spotify-exp">
               <img src={SPOTIFYLOGO} alt={title}></img> Spotify
@@ -66,7 +78,7 @@ function PlaylistPage({ title }) {
         </div>
         <div className="playlist-buttons">
           <div className="playlist-actions">
-            <img src={HEARTICON} alt={title}></img>
+            <img src={HEARTICON} onClick={handleOnLike} alt={title}></img>
             <img src={SHAREICON} alt={title}></img>
             <img src={DETAILSICON} alt={title}></img>
           </div>
