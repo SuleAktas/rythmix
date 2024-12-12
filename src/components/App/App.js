b9927703c4be9bcf766a1494ecd6b719729d01d6
SuleAktas <suleaktas@live.nl>
2024-12-12 07:29:45 +0100

    fix: unnecessary code remove

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,42 +1,29 @@
 import HomePage from "../HomePage/HomePage";
 import Footer from "../Layout/Footer/Footer";
-import Header from "../Layout/Header/Header";
 import SearchPage from "../SearchPage/SearchPage";
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PlaylistPage from "../PlaylistPage/PlaylistPage";
-import Song from "../Song/Song";
 import SongPlayer from "../SongPlayer/SongPlayer";
 import LibraryPage from "../LibraryPage/LibraryPage";
 import { AppProvider } from "../../providers/AppProvider";
 
 function App() {
-  const location = useLocation();
-
+  // TODO: song.js acildiginda footerin kapatilmasi gerekiyor.
   return (
-    <div
-      className={`app-container ${location.pathname === "/song" ? "full" : ""}`}
-    >
+    <div className={`app-container`}>
       <AppProvider>
         <div className="App">
-          {location.pathname === "/" && <Header />}
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/search" element={<SearchPage />} />
             <Route path="/playlist/:id" element={<PlaylistPage />}>
               <Route path="song/:songId" element={<PlaylistPage />} />
             </Route>
-
-            <Route path="/song" element={<Song />} />
             <Route path="/library" element={<LibraryPage />} />
           </Routes>
-          {location.pathname !== "/song" && <SongPlayer />}
-          {location.pathname !== "/song" && <Footer />}
+          <SongPlayer />
+          <Footer />
         </div>
       </AppProvider>
     </div>
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Slider from "../Slider/Slider";
+import Header from "../Layout/Header/Header";
 import "./HomePage.css";
 
 function HomePage() {
   return (
     <div className="home-page">
+      <Header />
       <Slider title="Reccomandations for Sule" />
       <Slider title="Voices from Indie Music" />
       <Slider title="Continue where you left" />
