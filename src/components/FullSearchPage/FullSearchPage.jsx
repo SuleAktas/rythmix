d812406903fc07f652616865812611e85313d853
SuleAktas <suleaktas@live.nl>
2025-04-14 20:36:36 +0200

    chore:cleanup

--- src/components/FullSearchPage/FullSearchPage.jsx
+++ src/components/FullSearchPage/FullSearchPage.jsx
@@ -1,47 +1,47 @@
-import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
-import PlaylistSong from "../PlaylistPage/components/PlaylistSong/PlaylistSong";
-import Filters from "../SearchPage/components/Filters/Filters";
-import SearchInput from "../SearchPage/components/SearchInput/SearchInput";
-import client from "../../services/client";
-import BackIcon from "../SVG/BackIcon";
-import "./FullSearchPage.css";
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import PlaylistSong from '../PlaylistPage/components/PlaylistSong/PlaylistSong';
+import Filters from '../SearchPage/components/Filters/Filters';
+import SearchInput from '../SearchPage/components/SearchInput/SearchInput';
+import client from '../../services/client';
+import BackIcon from '../SVG/BackIcon';
+import './FullSearchPage.css';
 
 function FullSearchPage({ onClose }) {
-  const [tracks, setTracks] = useState([]);
+	const [tracks, setTracks] = useState([]);
 
-  useEffect(() => {
-    client(
-      `https://api.jamendo.com/v3.0/tracks/?client_id=${
-        import.meta.env.VITE_JAMENDO_CLIENT_ID
-      }&tags=rock`,
-    ).then((response) => {
-      if (response.status === "Success") {
-        setTracks(response.data);
-      } else {
-        console.error("Tracks cannot be found!");
-      }
-    });
-  }, [setTracks]);
+	useEffect(() => {
+		client(
+			`https://api.jamendo.com/v3.0/tracks/?client_id=${
+				import.meta.env.VITE_JAMENDO_CLIENT_ID
+			}&tags=rock`
+		).then(response => {
+			if (response.status === 'Success') {
+				setTracks(response.data);
+			} else {
+				console.error('Tracks cannot be found!');
+			}
+		});
+	}, [setTracks]);
 
-  return (
-    <div className="full-search-container">
-      <div className="search-header">
-        <BackIcon className="back-icon" onClick={onClose} />
-        <SearchInput fullPage={true}></SearchInput>
-      </div>
-      <div className="filters">
-        <Filters></Filters>
-      </div>
-      <div className="filtered-songs">
-        {tracks &&
-          tracks.map((track) => {
-            return <PlaylistSong key={track.id} song={track} />;
-          })}
-      </div>
-    </div>
-  );
+	return (
+		<div className="full-search-container">
+			<div className="search-header">
+				<BackIcon className="back-icon" onClick={onClose} />
+				<SearchInput fullPage={true}></SearchInput>
+			</div>
+			<div className="filters">
+				<Filters></Filters>
+			</div>
+			<div className="filtered-songs">
+				{tracks &&
+					tracks.map(track => {
+						return <PlaylistSong key={track.id} song={track} />;
+					})}
+			</div>
+		</div>
+	);
 }
 
 export default FullSearchPage;
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,62 +1,61 @@
-import React from "react";
-import "./Slider.css";
-import Card from "../Card/Card";
-import { useEffect } from "react";
-import client from "../../services/client";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
-import { useState } from "react";
+import React from 'react';
+import './Slider.css';
+import Card from '../Card/Card';
+import { useEffect } from 'react';
+import client from '../../services/client';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+import { useState } from 'react';
 
 function Slider({ title }) {
-  console.log(import.meta.env.VITE_JAMENDO_CLIENT_ID);
-  const [albums, setAlbums] = useState([]);
+	const [albums, setAlbums] = useState([]);
 
-  const cardSkeleton = () => {
-    return (
-      <div>
-        <Skeleton
-          baseColor="#2b2b2b"
-          highlightColor="#3b3b3b"
-          height={152}
-          width={152}
-        />
-        <br />
-        <Skeleton
-          baseColor="#2b2b2b"
-          highlightColor="#3b3b3b"
-          height={20}
-          width={152}
-        />
-      </div>
-    );
-  };
+	const cardSkeleton = () => {
+		return (
+			<div>
+				<Skeleton
+					baseColor="#2b2b2b"
+					highlightColor="#3b3b3b"
+					height={152}
+					width={152}
+				/>
+				<br />
+				<Skeleton
+					baseColor="#2b2b2b"
+					highlightColor="#3b3b3b"
+					height={20}
+					width={152}
+				/>
+			</div>
+		);
+	};
 
-  useEffect(() => {
-    client(
-      `https://api.jamendo.com/v3.0/albums/?client_id=${
-        import.meta.env.VITE_JAMENDO_CLIENT_ID
-      }`,
-    ).then((response) => {
-      if (response.status === "Success") {
-        setAlbums(response.data);
-      } else {
-        console.error("Album cannot be found!");
-      }
-    });
-  }, [setAlbums]);
+	useEffect(() => {
+		client(
+			`https://api.jamendo.com/v3.0/albums/?client_id=${
+				import.meta.env.VITE_JAMENDO_CLIENT_ID
+			}`
+		).then(response => {
+			if (response.status === 'Success') {
+				setAlbums(response.data);
+			} else {
+				console.error('Album cannot be found!');
+			}
+		});
+	}, [setAlbums]);
 
-  return (
-    <div className="slider">
-      <div className="slider-title">{title}</div>
-      <div className="slider-container">
-        {albums.length !== 0
-          ? albums.map((album, index) => <Card key={index} album={album} />)
-          : [...Array(10)].map(() => {
-              return cardSkeleton();
-            })}
-      </div>
-    </div>
-  );
+	return (
+		<div className="slider">
+			<div className="slider-title">{title}</div>
+			<div className="slider-container">
+				{albums.length !== 0
+					? albums.map((album, index) => <Card key={index} album={album} />)
+					: [...Array(10)].map(() => {
+							return cardSkeleton();
+					  })}
+			</div>
+		</div>
+	);
 }
 
 export default Slider;
